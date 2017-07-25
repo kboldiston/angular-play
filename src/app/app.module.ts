bc3f5d60e4afef75b2581fd70cbc34ef75f3c234
Kyle Boldiston <k.boldiston@gmail.com>
2017-07-25 23:35:53 +1000

    initial boilerplate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MdToolbarModule, MdButtonModule, MdSidenavModule, MdListModule } from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdSidenavModule,
+    MdListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
